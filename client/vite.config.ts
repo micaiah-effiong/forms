ef098369e948871064d1261147898d67acc40f7c
Micah Effiong <micaiah.effiong@gmail.com>
2024-02-27 00:41:07 +0100

    add config for multi-page build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
+
+const rollupInputResolve = (...paths: string[]) => {
+  return resolve(__dirname, ...paths);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: rollupInputResolve("index.html"),
+        forms: rollupInputResolve("forms/index.html"),
+      },
+    },
+  },
   resolve: {
     alias: [
       {

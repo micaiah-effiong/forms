7e2da1c143a03b62c41872bb353d350b51bc9235
Micah Effiong <micaiah.effiong@gmail.com>
2024-02-27 00:19:01 +0100

    update form page style

--- client/src/components/table.tsx
+++ client/src/components/table.tsx
@@ -30,18 +30,28 @@ export const FormList = withQueryClientProvider(function () {
   console.log(formlistQuery.data);
 
   return (
-    <main className="h-screen w-full bg-white">
+    <main className="h-screen w-full pt-6 pr-3 bg-[#e6e6e6]">
       <div className="flex h-full">
-        <div className="w-2/12 p-4">
-          <button>create</button>
+        <div className="w-2/12 p-4 flex flex-col gap-3">
           <ul>
+            <li className="text-black font-bold text-lg my-4">Forms</li>
+          </ul>
+          <ul className="grid gap-2">
+            <li className="text-black font-bold">+ Create...</li>
+            <li className="truncate px-2 py-1 rounded-md bg-[#a259ff] font-semibold text-white">
+              Home
+            </li>
             {formlistQuery.data &&
               formlistQuery.data.map((form) => {
-                return <li key={form.id}> {form.name}</li>;
+                return (
+                  <li className="truncate px-2 py-1 rounded-md" key={form.id}>
+                    {form.name}
+                  </li>
+                );
               })}
           </ul>
         </div>
-        <div className="border-2 border-black w-full p-3 h-full">
+        <div className="bg-white shadow-md rounded-t-md  w-full p-3 h-full">
           <table>
             <thead>
               <tr>
